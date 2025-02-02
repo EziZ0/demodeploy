5e64d702cd09af302dae7ac264067d5541cdd204
ElzinM <elzinselvam@gmail.com>
2025-02-02 23:27:53 +0530

    cors updated

--- src/main/java/com/springboot/Sprinboot/Controller.java
+++ src/main/java/com/springboot/Sprinboot/Controller.java
@@ -2,13 +2,11 @@ package com.springboot.Sprinboot;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.Banner;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin(origins = "*")
 @RestController
 public class Controller {
 
